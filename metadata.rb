e9f5c36e866fba5308e2884423a238a5f6d82a7f
Brad Montgomery <brad@bradmontgomery.net>
2012-04-17 16:36:31 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bmontgomery@coroutine.com"
 license          "Apache 2.0"
 description      "Installs/Configures Kibana"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 recipe "kibana::default", "Downloads/Installs Kibana"
 recipe "kibana::apache",  "Creates and enables an apache site config for Kibana"
